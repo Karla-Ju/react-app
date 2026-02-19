144f9b4e5717da772311a2054a00fc38891f937b
Karla-Ju <karlajuau@gmail.com>
2026-02-19 15:25:20 -0500

    updated Message.tsx

--- src/Message.tsx
+++ src/Message.tsx
@@ -1,7 +1,7 @@
 // PascalCasing
 function Message() {
     // JSX: JavaScript XML
-    const name = 'Karla'
+    const name = 'name'
     // if (name)
         // return <h1>Hello {name}</h1>;
     return <h1>Hello World</h1>;
